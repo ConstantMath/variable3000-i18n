361ab78d626b2daaf6bdc5cbbf3dfabacae38140
Constant <constant@variable.club>
2016-12-02 09:40:21 +0100

    de v → ajout controller 'pages'

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,6 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'PagesController@home');
 
 Route::auth();
-Route::get('/admintool', 'AdmintoolController@index');
+Route::get('admintool', 'AdmintoolController@index');
 Route::resource('articles', 'ArticlesController');
