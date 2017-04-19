df51f5284cdb4190f0d6effc55da542cfcd55c37
Constant <constant@variable.club>
2017-04-19 14:36:10 +0200

    admin → article bt delete

--- resources/views/admin/components/article-delete-form.blade.php
+++ resources/views/admin/components/article-delete-form.blade.php
@@ -1,6 +1,6 @@
+<a href="#" class="" data-toggle="modal" data-target="#confirm-delete"><i class="fa fa-trash"></i> delete</a>
 {!! Form::model($article, ['route' => ['admin.articles.destroy', $article->id], 'method' => 'post', 'class' => 'form-horizontal', 'name' => 'delete-form']) !!}
   {{ Form::hidden('_method', 'DELETE') }}
-  <input type="button" name="btn" value="Delete this article" id="submitBtn" data-toggle="modal" data-target="#confirm-submit" class="btn btn-default" />
   <div class="modal fade" id="confirm-submit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
