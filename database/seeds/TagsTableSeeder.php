527964dfa4192079c1b77d934d647311d4a95cc0
Constant <constant@variable.club>
2017-05-16 10:19:07 +0200

    cp seeders

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('tags')->insert([
+          'name' => 'Categories',
+          'slug'  => 'categories',
+          'parent_id' => 0,
+      ]);
+
+      DB::table('tags')->insert([
+        'name' => 'Tags',
+        'slug'  => 'tags',
+        'parent_id' => 0,
+      ]);
+
+      DB::table('tags')->insert([
+        'name' => 'Categorie 01',
+        'slug' => 'categorie-01',
+        'parent_id' => 1,
+      ]);
+
+      DB::table('tags')->insert([
+        'name' => 'Tag 01',
+        'slug' => 'tag-01',
+        'parent_id' => 2,
+      ]);
+    }
+}
