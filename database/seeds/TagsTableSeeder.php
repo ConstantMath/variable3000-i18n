1441c8ff08b7888d2988bb52e454c570089e5a78
Constant <constant@variable.club>
2017-06-16 13:57:55 +0200

    seeder translated

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Tag;
 
 class TagsTableSeeder extends Seeder
 {
@@ -11,30 +12,30 @@ class TagsTableSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('tags')->insert([
-          'name' => 'Categories',
-          'slug'  => 'categories',
-          'parent_id' => 0,
-          'order' => 0,
-      ]);
-
-      DB::table('tags')->insert([
-        'name' => 'Tags',
-        'slug'  => 'tags',
+      Tag::create([
+        'en'  => ['name' => 'Categories', 'slug' => 'categories'],
+        'fr'  => ['name' => 'Categories', 'slug' => 'categories'],
         'parent_id' => 0,
         'order' => 0,
       ]);
 
-      DB::table('tags')->insert([
-        'name' => 'Categorie 01',
-        'slug' => 'categorie-01',
+      Tag::create([
+        'en'  => ['name' => 'Category', 'slug' => 'tags'],
+        'fr'  => ['name' => 'Categorie', 'slug' => 'tags'],
+        'parent_id' => 0,
+        'order' => 1,
+      ]);
+
+      Tag::create([
+        'en'  => ['name' => 'Categorie 1', 'slug' => 'categorie-1'],
+        'fr'  => ['name' => 'Categorie 1', 'slug' => 'categorie-1'],
         'parent_id' => 1,
         'order' => 0,
       ]);
 
-      DB::table('tags')->insert([
-        'name' => 'Tag 01',
-        'slug' => 'tag-01',
+      Tag::create([
+        'en'  => ['name' => 'Tag 01', 'slug' => 'tag-01'],
+        'fr'  => ['name' => 'Tag 01', 'slug' => 'tag-01'],
         'parent_id' => 2,
         'order' => 0,
       ]);
