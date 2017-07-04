91da5ab7c18745426f8567bd520d45693e690d41
Constant <constant@variable.club>
2017-07-04 14:23:46 +0200

    media panel styles

--- resources/views/admin/components/article-gallery-panel.blade.php
+++ resources/views/admin/components/article-gallery-panel.blade.php
@@ -1,14 +1,14 @@
 <div class="panel panel-default media-panel panel-mediagallery" id="panel-mediagallery">
   <div class="panel-heading">Gallery</div>
   <div class="panel-body">
-    <ul class="list-group" id="mediagallery">
+    <ul class="list-group @if(count($article->medias) == 0) ul-empty @endif" id="mediagallery">
+      <?php $media = array(); ?>
+      @include('admin.components.molecules-media-li')
       @if (isset($article->medias))
         @foreach ($article->medias as $media)
           @include('admin.components.molecules-media-li')
         @endforeach
       @endif
-      <?php $media = array(); ?>
-      @include('admin.components.molecules-media-li')
     </ul>
     @if (isset($article->id))
       {!! Form::model($article, ['route' => ['admin.articles.addmanymedia', $article->id], 'method' => 'post', 'class' => 'form-horizontal', 'id' => 'article-gallery-upload', 'autocomplete' => 'off', 'enctype' => 'multipart/form-data']) !!}
