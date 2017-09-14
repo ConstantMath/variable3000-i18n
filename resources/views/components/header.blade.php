7dbc3bbbd19f6960fdb529222a1c76952451322d
constant <constant@variable.club>
2017-09-14 13:22:19 +0200

    front dynamic lang switch

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,12 @@
 <!-- header -->
 <header class="primary-header">
+  @if(count(config('translatable.locales')) > 1 )
   <ul class="lang">
-    <li class="lang__item @if (app()->getLocale() == "fr") active @endif"><a href="{{ route('lang.switch', "fr") }}" class="lang__link">Fr</a></li>
-    <li class="lang__item @if (app()->getLocale() == "en") active @endif"><a href="{{ route('lang.switch', "en") }}" class="lang__link">En</a></li>
+    @foreach (config('translatable.locales') as $lang)
+      <li class="lang__item @if (app()->getLocale() ==  $lang) active @endif"><a href="{{ route('lang.switch', $lang) }}" class="lang__link">{{ $lang }}</a></li>
+    @endforeach
   </ul>
+  @endif
   <h1 class="primary-title">
     <a href="{{ url('/') }}" id="infos-button">Variable</a>
   </h1>
