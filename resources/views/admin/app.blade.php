d676465ed781094861067e6f2b71893c58ba894d
constant <constant@variable.club>
2018-05-29 14:26:52 +0200

    rm conflict

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -24,13 +24,8 @@
     @endif
   </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-<<<<<<< HEAD
-
   <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-=======
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
->>>>>>> 1211355dc552b517bc2b2dd0c81e7ae38400629f
   <script src="{{ url('/assets/admin/scripts.min.js') }}"></script>
   @yield('scripts')
 </body>
