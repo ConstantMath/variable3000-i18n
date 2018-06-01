755d33580d0974fd0528e64f5b2cca756596e58c
constant <constant@variable.club>
2018-06-01 17:10:16 +0200

    some positions

--- resources/views/admin/components/footer.blade.php
+++ resources/views/admin/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer__main">
   <ul>
     <li>Variable Web Admin</li>
-    <li><a href="mailto:bonjour@variable.club" class="link">Contact</a></li>
+    <li><a href="mailto:bonjour@variable.club" class="link">bonjour@variable.club</a></li>
   </ul>
 </footer>
