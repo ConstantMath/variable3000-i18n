7139ec22b7ce2cdc1424aed32f4836e4658fae19
Hugo <hugo@variable.club>
2018-06-15 14:38:13 +0200

    tabs fix + inutile js remove

--- public/assets/admin/scripts.js
+++ public/assets/admin/scripts.js
@@ -19879,9 +19879,6 @@ if ($('.panel.multiple').length) {
     })
 }
 
-
-
-
 $(document).on('click', 'a[data-toggle="modal"]', function(e) {
     e.preventDefault();
     $($(this).attr('data-target')).fadeIn(250);
@@ -19897,35 +19894,6 @@ $(document).keyup(function(e) {
         $(".modal").fadeOut(250);
     }
 });
-
-// if ( $('table #sortable').length ){
-//
-//   var list = document.getElementById("sortable");
-//   Sortable.create(list,{
-//     onUpdate: function (evt) {
-//       var mediatable_type = evt.item.getAttribute('data-mediatable-type');
-//       var article_id      = evt.item.getAttribute('data-article-id');
-//       var parent_id       = evt.item.getAttribute('data-parent-id');
-//       var new_order       = evt.newIndex;
-//       if (article_id && mediatable_type) {
-//         jQuery.ajax({
-//           url: admin_url + '/' + mediatable_type + '/reorder',
-//           data: {
-//             'id'        : article_id,
-//             'parent_id' : parent_id,
-//             'new_order' : new_order,
-//           },
-//           type: 'POST',
-//           success: function(response){
-//             if(response.status == 'success'){
-//               //$('<span class="message pull-right">Updated !</span>').appendTo(".panel-mediagallery .panel-heading").fadeOut(3000);
-//             }
-//           }
-//         });
-//       }
-//     }
-//   });
-// }
 
 $('.tab-select li').click(function(e) {
     $('.tab-select li').removeClass('active');
